f589478b73e13b57bf8f3cda80818ea8cb7c6929
ChetsadaSr1kun <66011211145@msu.ac.th>
2025-07-21 15:39:43 +0700

    Update index.php

--- src/main/index.php
+++ src/main/index.php
@@ -11,6 +11,12 @@
     <title>Document</title>
 </head>
 <body>
+    <div class="top-bar">
+        <div class="search-bar">
+            <input type="text" placeholder="ลูกค้าใหม่ รับเลย โค้ดส่วนลด หรือโค้ดส่งฟรี*">
+            <button>🔍</button>
+        </div>
+    </div>
     <h1>Hello World1</h1>
 </body>
 </html>
