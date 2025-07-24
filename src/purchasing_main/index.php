5b64613d28d5b011e28110c62d333e0238311f73
Tesspitch <testo3663@gmail.com>
2025-07-24 15:30:28 +0700

    แก้อีกคือเก่า

---
+++ src/purchasing_main/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include_once('../includes/header.php');
+    include_once('../includes/purchasing_sidebar.php');
+    include_once('../includes/bootstrap.php');
+    loadBootstrapCSS();
+    loadBootstrapJS();
+    ?> 
+    
+    <title>ADMIN</title>
+
+</head>
+
+<body>
+    <h1></h1>
+</body>
+</html>
